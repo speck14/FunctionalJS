eba2b7fa114905af55556c70628105e29dcd0b5d
Shannon Peck <shannon.peck@cornerstonenw.com>
2021-10-18 14:53:40 -0700

    'recursion' and 'basic-call' exercises complete

---
+++ basic-call.js
@@ -0,0 +1,8 @@
+'use strict'
+
+function duckCount () {
+  return Array.prototype.slice.call(arguments).filter(function(arg) {
+    return Object.prototype.hasOwnProperty.call(arg, 'quack')
+  }).length;
+}
+    module.exports = duckCount;
---
+++ recursion.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/*function countWords(inputWords) {
+  return inputWords.reduce(function(wordObj, curr) {
+    if(wordObj.hasOwnProperty(curr)){
+        wordObj[curr] +=1;
+    }
+    else {
+      wordObj[curr] = 1;
+    }
+    return wordObj;
+  }, {}) //empty object is passed for .reduce initial value parameter. Otherwise "curr" would be used as initial value, and first item in array would be skipped
+}
+module.exports = countWords;
+*/
+
+
+function reduce (arr, fn, initial) {
+  if(!arr.length) return initial;
+  let curr = arr[0]
+  initial = fn (initial, curr)
+  return reduce(arr.slice(1), fn, initial)
+}
+module.exports = reduce;
