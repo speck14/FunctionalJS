a11d9acb2c5ffbde02784c8fe7d30ce23c889166
Shannon Peck <shannon.peck@cornerstonenw.com>
2021-10-27 13:28:52 -0700

    Tested another implementation, cleaned up currying file

--- currying.js
+++ currying.js
@@ -15,3 +15,12 @@ function curryN(fn, n) {
   }
 }
 module.exports = curryN;
+
+/*
+Testing if the solution still works if arg is passed in as an argument to curryN works (it does)
+function curryN(fn, n, arg) {
+  n = n || fn.length;
+    if(n <= 1) return fn(arg)
+    return curryN(fn.bind(this, arg), n-1)
+  }
+  */
